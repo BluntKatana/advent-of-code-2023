40eeede5b1217b6ded1a627f3ed236957b993fa9
Floris Bos <57983252+BluntKatana@users.noreply.github.com>
2023-12-02 12:34:32 +0100

    day2: c++ both parts

---
+++ day-2/cpp/part1.cpp
@@ -0,0 +1,110 @@
+// To run do: c++ part1.cpp -Wall -std=c++17 -o outpu1.out && ./output1.out
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int print_string_vector(std::vector<std::string> str_vector)
+{
+    std::cout << str_vector.size() << " [";
+    for (std::string str : str_vector)
+    {
+        std::cout << "'" << str << "'"
+                  << ", ";
+    }
+
+    std::cout << "]" << std::endl;
+
+    return 0;
+}
+
+std::vector<std::string> splitter(std::string s, std::string seperator)
+{
+    std::vector<std::string> res;
+
+    // initialize index for seperator
+    // size_t as the result of s.find is size_t??
+    size_t pos_of_seperator;
+    pos_of_seperator = s.find(seperator);
+
+    while (pos_of_seperator != std::string::npos)
+    {
+        // Grab the substring
+        std::string substring = s.substr(0, pos_of_seperator);
+        // std::cout << s << "|" << substring << std::endl;
+
+        // Add substring to array and erase the substring from original string
+        res.push_back(substring);
+        s.erase(0, pos_of_seperator + seperator.length());
+
+        // find new seperator index
+        pos_of_seperator = s.find(seperator);
+        // std::cout << s << "|" << substring << "|" << 0 << ":" << pos_of_seperator << std::endl;
+    }
+
+    res.push_back(s);
+
+    return res;
+}
+
+int main()
+{
+    // Initialize a vector to store the file input and set the input file name.
+    std::vector<std::string> lines;
+    std::string fileName = "../input.txt";
+
+    // Read the file and store the result in the lines vector.
+    std::ifstream file(fileName);
+    if (file.is_open())
+    {
+        std::string line;
+        while (getline(file, line))
+        {
+            lines.push_back(line);
+        }
+        file.close();
+    }
+
+    map<string, int> MAP_MAX_PER_COLOR = {
+        {"red", 12}, {"green", 13}, {"blue", 14}};
+
+    int game_id_total = 0;
+
+    // Iterate over the lines vector and print each line.
+    for (std::string line : lines)
+    {
+        // Parse the game_id and sets of cubes
+        std::vector<std::string> game_id_and_cubes = splitter(line, ": ");
+        int game_id = stoi(splitter(game_id_and_cubes[0], " ")[1]);
+        std::vector<std::string> sets_of_cubes = splitter(game_id_and_cubes[1], "; ");
+
+        bool is_valid_set = true;
+        for (std::string set : sets_of_cubes)
+        {
+            std::vector<std::string> cubes_in_set = splitter(set, ", ");
+            for (std::string cube : cubes_in_set)
+            {
+                std::vector<std::string> split_cube = splitter(cube, " ");
+                int num = stoi(split_cube[0]);
+                std::string color = split_cube[1];
+
+                // Check if the cube amount if larger than the maximum allowed
+                if (num > MAP_MAX_PER_COLOR[color])
+                {
+                    is_valid_set = false;
+                }
+            }
+        }
+
+        if (is_valid_set)
+        {
+            game_id_total += game_id;
+        }
+    }
+
+    // Print the result.
+    std::cout << "Result: " << game_id_total << std::endl;
+}
