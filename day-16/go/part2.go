7f5c2799eaf10038541eeebdaac067c1f1c1dd59
Floris Bos <57983252+BluntKatana@users.noreply.github.com>
2023-12-16 12:01:45 +0100

    comment better

--- day-16/go/part2.go
+++ day-16/go/part2.go
@@ -21,24 +21,26 @@ func (d Day16) Part2(filename *string) string {
 	}
 	grid = MirrorGrid(grid)
 
+	// Initialize parallelism
+	var wg sync.WaitGroup
+	wg.Add(len(grid)*2 + len(grid[0])*2)
+
 	// Find the max energized
 	var maxEnergized = 0
 
-	var wg sync.WaitGroup
-
-	wg.Add(len(grid)*2 + len(grid[0])*2)
-
+	// Check the top and bottom sides
 	for x := 0; x < len(grid); x++ {
 		go FindEnergized(x, 0, 0, 1, grid, &maxEnergized, &wg)
 		go FindEnergized(x, len(grid)-1, 0, -1, grid, &maxEnergized, &wg)
 	}
 
-	// Check from left going right
+	// Check the left and right sides
 	for y := 0; y < len(grid[0]); y++ {
 		go FindEnergized(0, y, 1, 0, grid, &maxEnergized, &wg)
 		go FindEnergized(len(grid)-1, y, -1, 0, grid, &maxEnergized, &wg)
 	}
 
+	// Wait for all goroutines to finish
 	wg.Wait()
 
 	fmt.Println(time.Since(start))
