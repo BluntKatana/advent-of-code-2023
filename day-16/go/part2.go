a068f5f07fecbeece778718aad14659072fb143c
Floris Bos <57983252+BluntKatana@users.noreply.github.com>
2023-12-16 11:10:23 +0100

    very inefficient part 2 golang

--- day-16/go/part2.go
+++ day-16/go/part2.go
@@ -2,15 +2,71 @@ package day16
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
 func (d Day16) Part2(filename *string) string {
 	var start = time.Now()
 
-	// var content, _ = os.ReadFile(*filename)
-	// var lines = strings.Split(string(content), "\n")
+	var content, _ = os.ReadFile(*filename)
+	var lines = strings.Split(string(content), "\n")
+
+	// Create the grid with mirrors
+	var grid = make([][]rune, len(lines))
+	for i, line := range lines {
+		grid[i] = []rune(line)
+	}
+	grid = MirrorGrid(grid)
+
+	// Find the max energized
+	var maxEnergized = 0
+
+	// Check from top going down
+	for x := 0; x < len(grid); x++ {
+		var energized = FindEnergized(x, 0, 0, 1, grid)
+		if energized > maxEnergized {
+			maxEnergized = energized
+		}
+	}
+
+	// Check from bottom going up
+	for x := 0; x < len(grid); x++ {
+		var energized = FindEnergized(x, len(grid)-1, 0, -1, grid)
+		if energized > maxEnergized {
+			maxEnergized = energized
+		}
+	}
+
+	// Check from left going right
+	for y := 0; y < len(grid[0]); y++ {
+		var energized = FindEnergized(0, y, 1, 0, grid)
+		if energized > maxEnergized {
+			maxEnergized = energized
+		}
+	}
+
+	// Check from right going left
+	for y := 0; y < len(grid[0]); y++ {
+		var energized = FindEnergized(len(grid)-1, y, -1, 0, grid)
+		if energized > maxEnergized {
+			maxEnergized = energized
+		}
+	}
 
 	fmt.Println(time.Since(start))
-	return fmt.Sprint(0)
+	return fmt.Sprint(maxEnergized)
+}
+
+func FindEnergized(x, y, dx, dy int, lines [][]rune) int {
+	var positionMap = make(PositionMap)
+	var grid MirrorGrid = make([][]rune, len(lines))
+	for i, line := range lines {
+		grid[i] = []rune(line)
+	}
+	grid = MirrorGrid(grid)
+	grid.Traverse(x, y, dx, dy, &positionMap)
+	var energized = grid.GetEnergized(positionMap)
+	return energized
 }
